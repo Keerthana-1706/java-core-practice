773717d1d7075599a4109a3c54cc85ab43748340
Keerthana K <keerthanakrishnan9787@gmail.com>
2026-02-11 23:04:40 +0530

    Add StudentProfile and EmployeeProfile programs

---
+++ basics/StudentProfile.java
@@ -0,0 +1,18 @@
+public class StudentProfile {
+
+    public static void main(String[] args) {
+
+        String studentName = "Alex";
+        int studentAge = 21;
+        double studentCgpa = 8.5;
+        char studentSection = 'B';
+        boolean isActive = true;
+
+        System.out.println("------------- Student Profile --------------");
+        System.out.println("Name    : " + studentName);
+        System.out.println("Age     : " + studentAge);
+        System.out.println("CGPA    : " + studentCgpa);
+        System.out.println("Section : " + studentSection);
+        System.out.println("Active  : " + isActive);
+    }
+}
