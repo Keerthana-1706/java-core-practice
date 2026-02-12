fd9cd68e6a671958d779f14654605bba43f36a46
Keerthana K <keerthanakrishnan9787@gmail.com>
2026-02-12 12:17:40 +0530

    Add employee input program using Scanner and handle newline issue

---
+++ basics/EmployeeProfile.java
@@ -0,0 +1,19 @@
+public class EmployeeProfile {
+    public static void main(String[] args) {
+
+        
+        String employeeName = "John Smith";
+        int employeeId = 101;
+        double employeeSalary = 55000.75;
+        char departmentCode = 'A';
+        boolean isPermanent = true;
+
+        
+        System.out.println("----------- Employee Profile -----------");
+        System.out.println("Name          : " + employeeName);
+        System.out.println("Employee ID   : " + employeeId);
+        System.out.println("Salary        : " + employeeSalary);
+        System.out.println("Department    : " + departmentCode);
+        System.out.println("Permanent     : " + isPermanent);
+    }
+}
