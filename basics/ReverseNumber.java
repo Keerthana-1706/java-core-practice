606d4f0ffc8893563e948e2e8870bb6fea710d01
Keerthana K <keerthanakrishnan9787@gmail.com>
2026-02-17 12:27:23 +0530

    Update reverse number program to include digit count

--- basics/ReverseNumber.java
+++ basics/ReverseNumber.java
@@ -8,15 +8,21 @@ public class ReverseNumber {
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
+        int originalNumber = number; // store original
+
         int reverse = 0;
+        int count = 0;
 
         while (number > 0) {
             int digit = number % 10;
             reverse = reverse * 10 + digit;
             number = number / 10;
+            count++; // count digits
         }
 
+        System.out.println("Original number: " + originalNumber);
         System.out.println("Reversed number: " + reverse);
+        System.out.println("Number of digits: " + count);
 
         scanner.close();
     }
