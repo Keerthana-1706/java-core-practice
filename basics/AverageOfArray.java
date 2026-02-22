a9d51d5ed5fd6a69611949d640e3817187ce9253
Keerthana K <keerthanakrishnan9787@gmail.com>
2026-02-22 19:29:59 +0530

    Add program to calculate average of array elements

---
+++ basics/AverageOfArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AverageOfArray {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            sum = sum + arr[i];
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Average: " + average);
+
+        scanner.close();
+    }
+}
